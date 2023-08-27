ac7b61e34d0bdfeca4e9b69e49fb44a7e8508c98
bhuwan shrestha <sthabhuwan168@gmail.com>
2023-08-27 13:40:10 +0545

    final commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -65,7 +65,11 @@ const NavBar = () => {
      dark:text-light relative z-10 lg:px-16 md:px-12 sm:px-8'>
 
 
-        <button className='flex-col justify-center items-center 2xl:hidden xl:hidden lg:flex' onClick={handleClick} aria-label='hidden'>
+<button
+  className='flex-col justify-center items-center 2xl:hidden xl:hidden lg:flex z-10'
+  onClick={handleClick}
+  aria-label='Toggle Navigation Menu'
+>
             <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
             <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
             <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -15,6 +15,7 @@ const TransitionEffect = () => {
 <motion.div className='fixed top-0 bottom-0 right-full w-full h-screen z-40 bg-light'
     initial = {{x:"100%", width: "100%"}}
     animate = {{x:"0%", width: "0%"}} 
+ 
     transition = {{delay:0.2, duration:0.6, ease:"easeInOut"}}
     />
 
