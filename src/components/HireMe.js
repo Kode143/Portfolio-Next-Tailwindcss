fec0702db0863271d471c00a6258b0e5418395e3
bhuwan shrestha <sthabhuwan168@gmail.com>
2023-08-27 14:16:31 +0545

    final commit

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const HireMe = () => {
   return (
     <div className='fixed left-4 bottom-4
-    flex items-center justify-center overflow-hidden md:right-0 md:left-auto md:top-0 md:bottom-auto md:absolute lg:right-0 lg:left-auto lg:top-0 lg:bottom-auto lg:absolute xs:z-20
+    flex items-center justify-center overflow-hidden md:right-0 md:left-auto md:top-0 md:bottom-auto md:absolute lg:right-0 lg:left-auto lg:top-0 lg:bottom-auto lg:absolute xs:z-10
     sm:right-0'>
         <div className='w-48 h-auto flex items-center justify-center relative md:w-24 lg:w-24'>
         <CircularText className={'fill-dark animate-spin-slow dark:fill-light'} />
